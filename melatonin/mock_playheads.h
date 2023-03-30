79353ad3ccefeac1572d908725af3d9b54c8035d
Sudara <sudara@alonetone.com>
2023-03-30 22:31:09 +0200

    Allow position advancing on the mock playhead

--- melatonin/mock_playheads.h
+++ melatonin/mock_playheads.h
@@ -30,7 +30,19 @@ namespace melatonin
             return info;
         }
 
+        void setIsPlaying (bool isPlaying)
+        {
+            info.setIsPlaying (isPlaying);
+        }
+
+        void advancePosition (size_t numSamples)
+        {
+            auto samplesPerBeat = 44100.f * 60.f / info.getBpm().orFallback (120.f);
+            info.setPpqPosition (info.getPpqPosition().orFallback(0) + (float) numSamples / samplesPerBeat);
+        }
+
     private:
         PositionInfo info;
+        double sampleRate = 44100.0;
     };
 }
